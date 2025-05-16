965de4d2e8171b6416b68e2a5d0ae03e1747cda9
SpringFallUSA <springfallusa@example.com>
2025-05-16 07:41:57 +0545

    Remove package-lock.json to avoid conflicts with yarn.lock

--- src/redux/features/comments/commentApi.js
+++ src/redux/features/comments/commentApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 const commentApi = createApi({
   reducerPath: 'commentApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://springfallusa-hwbm.onrender.com/api/comments',  // ✅ Corrected here
+    baseUrl: 'https://springfallusa-76wx.onrender.com/api/comments',  // ✅ Corrected here
     credentials: 'include',
   }),
   tagTypes: ['Comments'],
